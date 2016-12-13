3e61e66fd399c44fb2fef61ddab6d026d6a4d026
Gisellabella <gisella@gmail.com>
2016-12-13 11:33:29 -0700

    cleaned up

--- env.js
+++ env.js
@@ -1,8 +1,5 @@
 // API key for Google Places
-<<<<<<< HEAD
+
 var apiKey = "AIzaSyAQpQYzOOln7oitDS1n256tmGpLihwWljs";
-=======
-var apiKey = "AIzaSyCDnY6AryzbZ1MWq3RvZDoOyWA7tQgJGIc";
->>>>>>> 4f6378dd6b4ff8a9bf869670271a0abd132bf2e4
 
 module.exports= apiKey;
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 // user.js
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var bcrypt   = require('bcrypt-nodejs');
 
 var UserSchema = new Schema ({
   name: String,
@@ -14,8 +14,14 @@ var UserSchema = new Schema ({
 
 var User = mongoose.model('User', UserSchema);
 
+User.methods.encrypt = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+User.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.local.password);
+};
 
 
-module.exports = User;
-
+module.exports = mongoose.model('User', User);
 
