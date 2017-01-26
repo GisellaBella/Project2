58da2cef66ba9cdcbb08a7f72803d90a5e447072
Gisellabella <gisella@gmail.com>
2017-01-26 16:58:59 -0700

    disable node_modules cache

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var User = require("./user");
 mongoose.connect( 
 					process.env.MONGODB_URI || 
-                  process.env.MONGOLAB_URI || 
+                  	process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
                   "mongodb://localhost/bikeFinderApp2");
 
